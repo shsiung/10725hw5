826fcc64cda4824c2aea56902061310b433ddaad
shsiung <shsiung@g.hmc.edu>
2017-11-17 15:24:36 -0500

    fixing

--- admm.m
+++ admm.m
@@ -15,7 +15,7 @@ r = length(A);
 Z1 = zeros(n,r);
 Z2 = zeros(n,r);
 Z3 = zeros(n,r-1);
-B = zeros(p,r);
+B =  zeros(p,r);
 U1 = zeros(n,r);
 U2 = zeros(n,r);
 U3 = zeros(n,r-1);
@@ -23,11 +23,12 @@ D = [-1, 1, 0;
      0, -1, 1];
 y1 = repmat(y,1,r);
 curr_best = 999;
+
 for i = 1 : iter
     B = updateB(X,y1,B,Z1,Z2,Z3,U1,U2,U3); 
-    Z1 = updateZ1(X,y1,B,Z1,Z2,Z3,U1,U2,U3,A); 
+    Z1 = updateZ1(X,y1,B,Z1,Z2,Z3,U1,U2,U3,A,rho); 
     Z2 = updateZ2(X,y1,B,Z1,Z2,Z3,U1,U2,U3,A,D);
-    Z3 = updateZ3(X,y1,B,Z1,Z2,Z3,U1,U2,U3,A,D);
+    Z3 = updateZ3(X,y1,B,Z1,Z2,Z3,U1,U2,U3,A,D,rho);
     U1 = U1 + y1-X*B-Z1;
     U2 = U2 + X*B-Z2;
     U3 = U3+Z2*D'-Z3;
@@ -37,7 +38,6 @@ for i = 1 : iter
     if (val(i) < curr_best)
         curr_best =val(i);
     end
-    
 end
-val
-fprint('Best objective: %f', curr_best);
+fprintf('Best objective: %f', curr_best);
+plot(val)
--- updateZ1.m
+++ updateZ1.m
@@ -1,11 +1,13 @@
-function [ Z1new ] = updateZ1(X,y1,B,Z1,Z2,Z3,U1,U2,U3,A)
+function [ Z1new ] = updateZ1(X,y1,B,Z1,Z2,Z3,U1,U2,U3,A,rho)
     Z1new = zeros(size(Z1,1),size(Z1,2));
     for i = size(Z1,1)
         for j = size(Z1,2)
-            if Z1(i,j)>A(j)
-                Z1new(i,j) = Z1(i,j)-A(j);
-            else if Z1(i,j) < -(A(j)-1)
-                    Z1new(i,j) = Z1(i,j)-(A(j)-1);
+            cond = y1-X*B+U1;
+            if Z1(i,j)>1/rho*A(j)
+                Z1new(i,j) = cond(i,j)-1/rho*A(j);
+            else
+                if Z1(i,j) < -1/rho*(A(j)-1)
+                    Z1new(i,j) = cond(i,j)-1/rho*(A(j)-1);
                 else 
                     Z1new(i,j) = 0;
                 end
